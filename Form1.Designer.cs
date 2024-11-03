5b87591155a472fcfe62f722b66d414741905172
milan <milan@YULIA>
2024-11-03 23:54:28 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace CompressFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.selectFileBtn = new System.Windows.Forms.Button();
+            this.showFileNameTxt = new System.Windows.Forms.TextBox();
+            this.compressBtn = new System.Windows.Forms.Button();
+            this.decompressBtn = new System.Windows.Forms.Button();
+            this.savedSpacelbl = new System.Windows.Forms.Label();
+            this.compressedFileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LostSpaceLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // selectFileBtn
+            // 
+            this.selectFileBtn.Location = new System.Drawing.Point(638, 26);
+            this.selectFileBtn.Name = "selectFileBtn";
+            this.selectFileBtn.Size = new System.Drawing.Size(101, 23);
+            this.selectFileBtn.TabIndex = 0;
+            this.selectFileBtn.Text = "Datei auswahlen";
+            this.selectFileBtn.UseVisualStyleBackColor = true;
+            this.selectFileBtn.Click += new System.EventHandler(this.selectFileBtn_Click);
+            // 
+            // showFileNameTxt
+            // 
+            this.showFileNameTxt.Location = new System.Drawing.Point(92, 24);
+            this.showFileNameTxt.Name = "showFileNameTxt";
+            this.showFileNameTxt.Size = new System.Drawing.Size(349, 20);
+            this.showFileNameTxt.TabIndex = 1;
+            // 
+            // compressBtn
+            // 
+            this.compressBtn.Location = new System.Drawing.Point(638, 84);
+            this.compressBtn.Name = "compressBtn";
+            this.compressBtn.Size = new System.Drawing.Size(101, 23);
+            this.compressBtn.TabIndex = 2;
+            this.compressBtn.Text = "Komprimieren";
+            this.compressBtn.UseVisualStyleBackColor = true;
+            this.compressBtn.Click += new System.EventHandler(this.compressBtn_Click);
+            // 
+            // decompressBtn
+            // 
+            this.decompressBtn.Location = new System.Drawing.Point(638, 55);
+            this.decompressBtn.Name = "decompressBtn";
+            this.decompressBtn.Size = new System.Drawing.Size(101, 23);
+            this.decompressBtn.TabIndex = 3;
+            this.decompressBtn.Text = "Dekomprimieren";
+            this.decompressBtn.UseVisualStyleBackColor = true;
+            this.decompressBtn.Click += new System.EventHandler(this.decompressBtn_Click);
+            // 
+            // savedSpacelbl
+            // 
+            this.savedSpacelbl.AutoSize = true;
+            this.savedSpacelbl.Location = new System.Drawing.Point(467, 31);
+            this.savedSpacelbl.Name = "savedSpacelbl";
+            this.savedSpacelbl.Size = new System.Drawing.Size(0, 13);
+            this.savedSpacelbl.TabIndex = 4;
+            // 
+            // compressedFileName
+            // 
+            this.compressedFileName.Location = new System.Drawing.Point(92, 84);
+            this.compressedFileName.Name = "compressedFileName";
+            this.compressedFileName.Size = new System.Drawing.Size(349, 20);
+            this.compressedFileName.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Datei:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Ziel:";
+            // 
+            // LostSpaceLbl
+            // 
+            this.LostSpaceLbl.AutoSize = true;
+            this.LostSpaceLbl.Location = new System.Drawing.Point(467, 91);
+            this.LostSpaceLbl.Name = "LostSpaceLbl";
+            this.LostSpaceLbl.Size = new System.Drawing.Size(0, 13);
+            this.LostSpaceLbl.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 147);
+            this.Controls.Add(this.LostSpaceLbl);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.compressedFileName);
+            this.Controls.Add(this.savedSpacelbl);
+            this.Controls.Add(this.decompressBtn);
+            this.Controls.Add(this.compressBtn);
+            this.Controls.Add(this.showFileNameTxt);
+            this.Controls.Add(this.selectFileBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button selectFileBtn;
+        private System.Windows.Forms.TextBox showFileNameTxt;
+        private System.Windows.Forms.Button compressBtn;
+        private System.Windows.Forms.Button decompressBtn;
+        private System.Windows.Forms.Label savedSpacelbl;
+        private System.Windows.Forms.TextBox compressedFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LostSpaceLbl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Compression;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompressFiles
+{
+    public partial class Form1 : Form
+    {
+        private string _path;
+        public Form1()
+        {
+            InitializeComponent();
+            compressBtn.Enabled = false;
+            decompressBtn.Enabled = false;
+            compressedFileName.ReadOnly = true;
+            showFileNameTxt.ReadOnly = true;
+        }
+
+        private void selectFileBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog(); 
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+             _path = openFileDialog.FileName; showFileNameTxt.Text = _path;
+            compressBtn.Enabled = true;
+            decompressBtn.Enabled = true;
+        }
+
+        private void compressBtn_Click(object sender, EventArgs e)
+        {
+            if (_path == null) 
+            { 
+                MessageBox.Show("Error"); return; 
+            }
+            string compressedFilePath = _path + ".gz";
+            compressedFileName.Text = compressedFilePath;
+            FileStream originalFileStream = new FileStream(_path, FileMode.Open, FileAccess.Read);
+            FileStream compressedFileStream = new FileStream(compressedFilePath, FileMode.Create);
+            GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress);
+            
+                originalFileStream.CopyTo(compressionStream); 
+
+            CompressingBytes(_path, compressedFilePath);
+            compressionStream.Close();
+            compressedFileStream.Close();
+            originalFileStream.Close();
+           
+           
+
+        }
+        private void decompressBtn_Click(object sender, EventArgs e)
+        {
+            if (_path == null)
+            {
+                MessageBox.Show("Error"); return;
+            }
+            string decompressedFilePath = _path.Replace(".gz", "");
+            compressedFileName.Text= decompressedFilePath;
+           FileStream compressedFileStream = new FileStream(_path, FileMode.Open);
+
+            FileStream decompressedFileStream = new FileStream(decompressedFilePath, FileMode.Create);
+
+            GZipStream decompressionStream = new GZipStream(compressedFileStream, CompressionMode.Decompress);
+            {
+                decompressionStream.CopyTo(decompressedFileStream);
+            }
+            DecompressingBytes(decompressedFilePath, _path);
+            decompressionStream.Close();
+            decompressedFileStream.Close();
+            compressedFileStream.Close();
+
+        }
+
+      
+        private void CompressingBytes(string originalFilePath, string compressedFilePath)
+        {
+            FileInfo originalFile = new FileInfo(originalFilePath); 
+            FileInfo compressedFile = new FileInfo(compressedFilePath); 
+            double savings = (originalFile.Length- compressedFile.Length);
+            savedSpacelbl.Text = $"{savings} bytes";
+
+        }
+        private void DecompressingBytes(string originalFilePath, string compressedFilePath)
+        {
+            FileInfo originalFile = new FileInfo(originalFilePath);
+            FileInfo compressedFile = new FileInfo(compressedFilePath);
+            double savings = (originalFile.Length - compressedFile.Length);
+            LostSpaceLbl.Text = $"{savings} bytes";
+
+        }
+
+
+    }
+}
